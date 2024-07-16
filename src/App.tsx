ef19aa58bd1088091ae04603fd4dac9726ccd633
OlexKov <Kovalchuk.olex@gmail.com>
2024-07-16 13:30:53 +0300

    Route,bootstrap and AntDesign added

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
 import React from 'react';
-import Header from './components/header';
-import Sidebar from './components/sidebar';
-import MainContent from './components/main_content';
-import Footer from './components/footer';
 import './styles.css';
+import Layout from './components/layout';
+import { Route, Routes } from 'react-router-dom';
 
 const App: React.FC = () => {
   return (
-    <div className="app">
-      <Header />
-      <div className="main-content">
-        <Sidebar />
-        <MainContent />
-      </div>
-      <Footer />
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+                
+      </Route>
+    </Routes>
   );
 };
 
